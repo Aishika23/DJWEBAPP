e2879df327310a066c9980955468afb18abf7396
Aishika23 <100014326+Aishika23@users.noreply.github.com>
2023-03-14 19:56:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftWristY = 0;
+leftWristX = 0;
+rightWristY = 0;
+rightWristX = 0;
 
 function preload()
 {
@@ -9,13 +13,36 @@ song2 = loadSound("sound.mp3");
 
 function setup()
 {
-    canvas = createCanvas(600, 600);
-    canvas.position(560, 150);
+    canvas = createCanvas(500, 300);
+    canvas.position(500,250)
 
     video = createCapture(VIDEO);
     video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded());
+    posenet.on('pose', gotPoses);
 }
 
-function draw() {
-    image(video, 0, 0, 600, 600);
+function modelLoaded() 
+{
+    console.log("Model Initialized")
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = "+leftWristX+" Left Wrist Y = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = "+rightWristX+" Right Wrist Y = "+rightWristY);
+    }
+}
+
+function draw() 
+{
+    image(video, 0, 0, 500, 300);
 }
